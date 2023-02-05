c01da3a65a2abc76470153c97be2f9ee723026f3
Anastasiya P <anastasiapatsei@gmail.com>
2023-02-05 13:04:06 +0200

    solved

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int maxNumber;
+        maxNumber = (first > second && first > third) ?
+                (first > third ? first : second) :
+                (second > third ? second : third);
+
+        System.out.println(maxNumber);
+
     }
+
+    /* if (first > second && first > third){
+            System.out.println(first);
+        } else if (second > first && second > third){
+            System.out.println(second);
+        } else{
+            System.out.println(third);
+        }
+
+     */
 }
